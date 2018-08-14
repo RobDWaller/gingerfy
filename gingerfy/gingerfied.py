de8184e4498a37e3e0dee4deeb1fd374a75b691d
RobDWaller <rdwaller1984@googlemail.com>
2018-08-14 10:33:57 +0100

    Initial code commit for gingerfy library

---
+++ gingerfy/gingerfied.py
@@ -0,0 +1,19 @@
+class Gingerfied:
+
+    def __init__(self, fix, broken, fixes):
+
+        self.fix = fix
+        self.broken = broken
+        self.fixes = fixes
+
+    def getFix(self):
+
+        return self.fix
+
+    def getBroken(self):
+
+        return self.broken
+
+    def getFixes(self):
+
+        return self.fixes 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gingerfy',
+      version='0.1.0',
+      description='Replace all colours in a sentence with ginger',
+      url='http://github.com/RobDWaller/gingerfy',
+      author='RobDWaller',
+      author_email='rdwaller1984@googlemail.com',
+      license='MIT',
+      packages=['gingerfy'],
+      zip_safe=False)
